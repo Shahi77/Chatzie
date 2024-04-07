d881ec2dc451c84b76a127ecfce64e88dd822d5e
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-04-07 23:49:00 +0530

    setup server side code structure

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import connectDb from "./db/connect.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "../.env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDb()
+  .then(() => {
+    // check for errors
+    app.on("error", (error) => {
+      console.error("\nError connecting to server: ", error);
+      throw error;
+    });
+    // start the server
+    app.listen(port, () => {
+      console.log(`\nServer is listening at port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("\nMongoDB connection failed! ", err);
+  });
---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+// higher order function getting async function as an argument
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+export default asyncHandler;
